d6d1bd583b916e9af1866dbdd33a26879780d132
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-04-23 11:08:43 +0800

    add SEO

---
+++ main/components/Layout/SEO.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import {useContext} from 'react'
+import PageDataContext from '../../../utils/PageDataContext'
+
+export default function SEO() {
+	const {seoTitle, seoDescription, seoBannerUrl} = useContext(PageDataContext)
+	const fullSeoTitle = `${seoTitle} | Powered by Websheets`
+	const siteDomain = 'lioncitymakers.com'
+	const fullSiteUrl = `https://${siteDomain}`
+	return (
+		<Head>
+			<link rel="icon" href="/favicon.ico" />
+			{/* HTML Meta Tags */}
+			<title>{fullSeoTitle}</title>
+			<meta name="description" content={seoDescription} />
+
+			{/* Facebook Meta Tags */}
+			<meta property="og:url" content={fullSiteUrl} />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content={fullSeoTitle} />
+			<meta property="og:description" content={seoDescription} />
+			<meta property="og:image" content={seoBannerUrl} />
+
+			{/* Twitter Meta Tags */}
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta property="twitter:domain" content={siteDomain} />
+			<meta property="twitter:url" content={fullSiteUrl} />
+			<meta name="twitter:title" content={fullSeoTitle} />
+			<meta name="twitter:description" content={seoDescription} />
+			<meta name="twitter:image" content={seoBannerUrl} />
+		</Head>
+	)
+}
--- main/components/Layout/index.tsx
+++ main/components/Layout/index.tsx
@@ -1,6 +1,9 @@
+import SEO from "./SEO";
+
 export default function Layout({children}) {
 	return (
 		<div className="text-gray-900 dark:bg-black dark:text-white">
+			<SEO />
 			<div className="container mx-auto max-w-5xl py-4 px-4 flex flex-col min-h-screen">
 				<main className="flex-grow">{children}</main>
 			</div>
