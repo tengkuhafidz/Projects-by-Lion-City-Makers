72d133c4be24ba01a3abd62bab91aaf0e1af689e
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-04-25 09:21:08 +0800

    update form link

--- pages/submit.tsx
+++ pages/submit.tsx
@@ -3,7 +3,7 @@ export default () => null
 export const getServerSideProps = async context => {
 	return {
 		redirect: {
-			destination: `https://docs.google.com/forms/d/e/1FAIpQLScCs9xogXr2rLZ6fx4FcwKnpEY_Q5q2DIG1nspf4zGi4Ghefw/viewform?usp=sf_link`,
+			destination: `https://docs.google.com/forms/d/e/1FAIpQLSest-6iDMSQ4_LWTZO0XtdqbODv7gcwfQR5R6KUsgtftEiVxw/viewform?usp=sf_link`,
 			permanent: false,
 		},
 	}
